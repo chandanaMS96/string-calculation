62062899d0b90c125e8a5fd1c00ad000d0a23fdd
chandanams96 <chandanams96@gmail.com>
2025-06-25 17:29:52 +0530

    handle all kinds of delimiters

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
-function Add(input) {
+const Add = function (input) {
   if (!input) return 0;
-  const numbers = input.split(",").map(Number);
+  const numbers = extractNumbers(input);
   const negatives = numbers.filter((n) => n < 0);
   if (negatives.length > 0) {
     throw new Error("negative number is not allowed");
@@ -8,6 +8,15 @@ function Add(input) {
   const sum = numbers.filter((n) => n <= 1000).reduce((sum, n) => sum + n, 0);
   console.log(sum);
   return sum;
-}
+};
 
-Add("1,2,3");
+const extractNumbers = function (str) {
+  return (
+    str
+      .match(/\d+/g)
+      ?.map(Number)
+      .filter((n) => n <= 1000) || []
+  );
+};
+
+Add("//1,2***,3400");
