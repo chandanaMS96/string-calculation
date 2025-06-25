c56ec1bbd93db3ba03037c5a1a15dce76b272e81
chandanams96 <chandanams96@gmail.com>
2025-06-25 17:13:59 +0530

    basic number string calculation

---
+++ add.js
@@ -0,0 +1,13 @@
+function Add(input) {
+  if (!input) return 0;
+  const numbers = input.split(",").map(Number);
+  const negatives = numbers.filter((n) => n < 0);
+  if (negatives.length > 0) {
+    throw new Error("negative number is not allowed");
+  }
+  const sum = numbers.filter((n) => n <= 1000).reduce((sum, n) => sum + n, 0);
+  console.log(sum);
+  return sum;
+}
+
+Add("1,2,3");
